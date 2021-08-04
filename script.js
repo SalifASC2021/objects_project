1cccc3c30a04520f56c9cbf265da2b1ad7d0679a
SalifASC2021git push -u origin mastercleargit config --global user.name SalifASC2021 <salif.sangare@compscihigh.org>
2021-08-04 12:15:14 -0400

    4 people

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
+let profileDiv = document.querySelector('#content');
+let firstPerson = document.querySelector("#first");
+let secondPerson = document.querySelector("#second");
+let thirdPerson = document.querySelector("#third");
+let fourthPerson = document.querySelector("#fourth");
+
+function profileSetup(personNum) {
+    
+    profileDiv.innerHTML = "";
+    let newDiv = document.createElement("div");
+    let p1 = document.createElement("p");
+    let p2 = document.createElement("p");
+    let p3 = document.createElement("p");
+    let button = document.createElement("button");
+    let buttonDiv = document.createElement("div");
+    
+    p1.innerHTML = "My name is "+ info[personNum].firstName + " " + info[personNum].lastName;
+    p2.innerHTML = "I am " + info[personNum].heightFeet + " feet " + info[personNum].heighInches + " inches";
+    p3.innerHTML = "My favorite food is " + info[personNum].favoriteFood + " and my favorite color is " + info[personNum].favoriteColor;
+
+  
+    newDiv.style.color = "white";
+    newDiv.style.fontSize = "50px";
+    newDiv.style.margin = "5% 20% 5% 20%";
+    newDiv.style.padding = "30px";
+    newDiv.style.border = "solid black 5px";
+    
+    
+
+   
+    newDiv.appendChild(p1);
+    newDiv.appendChild(p2);
+    newDiv.appendChild(p3);
+    newDiv.appendChild(buttonDiv);
+    profileDiv.appendChild(newDiv);
+}
+
+firstPerson.onclick = function(event) {
+    event.preventDefault();
+    profileSetup(0);
+    
+    
+}
+secondPerson.onclick = function(event) {
+    event.preventDefault();
+    profileSetup(1);
+    
+}
+thirdPerson.onclick = function(event) {
+    event.preventDefault();
+    profileSetup(2);
+  
+}
+fourthPerson.onclick = function(event) {
+    event.preventDefault();
+    profileSetup(3);
+    
+}
+
+
 let info = {
     0: {
         firstName: "Francisco",
@@ -11,12 +71,36 @@ let info = {
         }
     },
     1: {
-        // Person #2
+        firstName: "Salif",
+        lastName: "Sangare",
+        heightFeet: "5",
+        heighInches: "10",
+        favoriteFood: "salad",
+        favoriteColor: "green",
+        sayFullName: function() {
+            alert("This is " + this.firstName + " " + this.lastName);
+        }
     },
     2: {
-        // Person #3
+        firstName: "james",
+        lastName: "bronk",
+        heightFeet: "6",
+        heighInches: "0",
+        favoriteFood: "fries",
+        favoriteColor: "blue",
+        sayFullName: function() {
+            alert("This is " + this.firstName + " " + this.lastName);
+        }
     },
     3: {
-        // Person #4
+        firstName: "bill",
+        lastName: "jack",
+        heightFeet: "5",
+        heighInches: "7",
+        favoriteFood: "pepper",
+        favoriteColor: "orange",
+        sayFullName: function() {
+            alert("This is " + this.firstName + " " + this.lastName);
+        }
     },
 }
